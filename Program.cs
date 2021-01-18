dcac6d7e55c6f59c5d86552202ee7187b3940e1a
Luciano Robe <lucodifier@gmail.com>
2021-01-18 10:25:53 -0300

    Projeto inicial

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Extensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adicionando zeros a esquerda");
+            var texto = "20";
+            Console.WriteLine(AddZeroToLeft(texto, 5));
+        }
+
+        public static string AddZeroToLeft(string input, int lenght)
+        {
+            if (!string.IsNullOrEmpty(input))
+            {
+                input = input.PadLeft(lenght, '0');
+            }
+            return input;
+        }
+    }
+}
